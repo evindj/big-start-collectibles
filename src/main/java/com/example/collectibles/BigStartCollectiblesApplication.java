60630db888a86c35338f8025e29b37c3de042b0b
Innocent Djiofack <idjiofack@Innocents-MacBook-Pro-2.local>
2023-11-26 13:02:52 -0800

    Improved scalability by using Async processing

--- src/main/java/com/example/collectibles/BigStartCollectiblesApplication.java
+++ src/main/java/com/example/collectibles/BigStartCollectiblesApplication.java
@@ -2,12 +2,27 @@ package com.example.collectibles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class BigStartCollectiblesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BigStartCollectiblesApplication.class, args);
+	}
+
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Spring-MVC-Async");
+		executor.initialize();
+		return executor;
 	}
 
 }
--- src/main/java/com/example/collectibles/controllers/ProductController.java
+++ src/main/java/com/example/collectibles/controllers/ProductController.java
@@ -2,20 +2,31 @@ package com.example.collectibles.controllers;
 
 import com.example.collectibles.beans.Product;
 import com.example.collectibles.dao.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 @Controller
 public class ProductController {
 
     private ProductRepository productRepository;
+    // has to be the same name as the bean definition.
+    private Executor asyncExecutor;
 
-    public ProductController(ProductRepository productRepository) {
+    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    public ProductController(ProductRepository productRepository,
+                             Executor asyncExecutor) {
         this.productRepository = productRepository;
+        this.asyncExecutor = asyncExecutor;
     }
     @PostMapping("/search")
     public String search(@RequestParam("searchString") String keyword, Model model) {
@@ -23,5 +34,25 @@ public class ProductController {
         model.addAttribute("products", products);
         model.addAttribute("searchedFor", keyword);
         return "search-results";
+    }
+
+    @GetMapping("/getAllProducts")
+    public DeferredResult<String> getAllProducts(Model model) {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        asyncExecutor.execute(()->{
+            model.addAttribute("products", getProducts());
+            deferredResult.setResult("product-list");
+        });
+        return deferredResult;
+    }
+    private Iterable<Product> getProducts() {
+        logger.info("Getting all products we are on spring executor thread");
+        try {
+            Thread.sleep(6000);
+        }
+        catch (InterruptedException exception) {
+            throw new RuntimeException();
+        }
+        return productRepository.findAll();
     }
 }
